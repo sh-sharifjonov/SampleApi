3fc9ce468c042319ae25acb90a22bfad9ad53aa6
HP <s.sharifjonov@eskhata.tj>
2025-10-14 21:45:12 +0500

    add main route

--- SampleApi/Program.cs
+++ SampleApi/Program.cs
@@ -21,6 +21,8 @@ var summaries = new[]
     "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
 };
 
+app.MapGet("/", () => "Welcome to the Sample API!");
+
 app.MapGet("/weatherforecast", () =>
     {
         var forecast = Enumerable.Range(1, 5).Select(index =>
